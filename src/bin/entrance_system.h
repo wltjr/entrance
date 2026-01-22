2f092d788ec34f62b4e4e596ca2bb4c58283abca
OxusByte45 <oxusbyte45@users.noreply.github.com>
2026-01-22 15:22:28 -0600

    client: Add distro system detection support

---
+++ src/bin/entrance_system.h
@@ -0,0 +1,8 @@
+#ifndef ENTRANCE_SYSTEM_H_
+#define ENTRANCE_SYSTEM_H_
+
+const char *entrance_system_hostname_get(void);
+const char *entrance_system_distro_get(void);
+const char *entrance_system_logo_get(void);
+
+#endif /* ENTRANCE_SYSTEM_H_ */
