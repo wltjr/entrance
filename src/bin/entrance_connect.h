eb358ebb05f6cf42de7bc2148d86ab4136f4f171
William L. Thomson Jr. <w@wltjr.com>
2026-01-29 18:41:50 -0500

    bin: Fix smell, make parameters pointer-to-const

--- src/bin/entrance_connect.h
+++ src/bin/entrance_connect.h
@@ -6,8 +6,8 @@ typedef void (*Entrance_Connect_Auth_Cb)(void *data, const char *login, Eina_Boo
 Eina_Bool entrance_connect(int port);
 void entrance_connect_auth_send(const char *login, const char *password, const char *session, Eina_Bool open_session);
 void entrance_connect_action_send(unsigned char id);
-void entrance_connect_conf_gui_send(Entrance_Conf_Gui_Event *conf);
-void entrance_connect_conf_user_send(Entrance_Login *el);
+void entrance_connect_conf_gui_send(const Entrance_Conf_Gui_Event *conf);
+void entrance_connect_conf_user_send(const Entrance_Login *el);
 void *entrance_connect_auth_cb_add(Entrance_Connect_Auth_Cb func, void *data);
 void entrance_connect_auth_cb_del(void *list);
 void entrance_connect_shutdown(void);
