d22e6228a7d3a85f6b00b8a5e62ff8f808613c07
William L. Thomson Jr. <w@wltjr.com>
2026-03-13 10:13:46 -0400

    daemon: Remove proto long entrance_session_seed_get(void);

--- src/daemon/entrance_session.h
+++ src/daemon/entrance_session.h
@@ -10,7 +10,6 @@ void entrance_session_close(Eina_Bool opened);
 Eina_Bool entrance_session_login(const char *command, Eina_Bool push);
 void entrance_session_pid_set(pid_t pid);
 pid_t entrance_session_pid_get(void);
-long  entrance_session_seed_get(void);
 char *entrance_session_login_get(void);
 int entrance_session_logged_get(void);
 Eina_List *entrance_session_list_get(void);
