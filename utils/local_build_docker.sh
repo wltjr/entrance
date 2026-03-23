fa0160e4ad11f30278f1b7f5f2cc237b8943ca24
William L. Thomson Jr. <w@wltjr.com>
2026-03-23 10:03:11 -0400

    utils: Only change vt, display, and socket port when DISPLAY=:0* is set
    - Run with defaults otherwise, it still hijacks the display at times with
      the change if running on live system, so still needs work beyond sed.

--- utils/local_build_docker.sh
+++ utils/local_build_docker.sh
@@ -10,8 +10,11 @@ meson setup --prefix /usr --buildtype=debug -Dpam=true -Dlogind=true . build
 ninja -C build install
 
 # prevent local X11 conflicts, vt, display, and socket port
-sed -i -e "s|7|9|" -e "s|:0|:1.0|" -e "s|42|43|" \
-	-e "s|Enlightenment|Xsession|" /etc/entrance/entrance.conf
+if [[ "${DISPLAY}" == \:0* ]]; then
+    sed -i -e "s|7|9|" -e "s|:0|:1.0|" -e "s|42|43|" /etc/entrance/entrance.conf
+fi
 
 # create xsession, directory and desktop file
 "$(dirname $0)/create_xsession.sh"
+
+sed -i -e "s|Enlightenment|Xsession|" /etc/entrance/entrance.conf
