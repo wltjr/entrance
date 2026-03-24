e86fa3e9e322775d038b86e3e8517ad53f959b8a
William L. Thomson Jr. <w@wltjr.com>
2026-03-24 10:17:23 -0400

    daemon: Correct preprocessor header name to match filename

--- src/daemon/entrance_xserver.h
+++ src/daemon/entrance_xserver.h
@@ -1,6 +1,8 @@
-#ifndef ENTRANCE_XLIB_H_
-#define ENTRANCE_XLIB_H_
+#ifndef ENTRANCE_XSERVER_H_
+#define ENTRANCE_XSERVER_H_
+
 typedef void (*Entrance_X_Cb)(const char *data);
 int entrance_xserver_init(Entrance_X_Cb start, const char *dname);
 void entrance_xserver_shutdown(void);
-#endif /* ENTRANCE_XLIB_H_ */
+
+#endif /* ENTRANCE_XSERVER_H_ */
