d2489999d58dea9cf3c87e7ec1c6db977ff00318
William L. Thomson Jr. <w@wltjr.com>
2026-03-26 10:26:03 -0400

    daemon: Add function proto void entrance_xservers_init(int count);
    - Used to allocate memory for multiple Entrance_Xserver structs vs just
      the one currently hard coded.

--- src/daemon/entrance_xserver.h
+++ src/daemon/entrance_xserver.h
@@ -3,6 +3,14 @@
 
 typedef void (*Entrance_X_Cb)(const char *data);
 int entrance_xserver_init(Entrance_X_Cb start, const char *dname);
+
+/**
+ * @brief Allocate memory for Entrance_Xserver structs, one per X server
+ *
+ * @param count the number of servers to allocate memory
+ */
+void entrance_xservers_init(int count);
+
 void entrance_xserver_shutdown(void);
 
 #endif /* ENTRANCE_XSERVER_H_ */
