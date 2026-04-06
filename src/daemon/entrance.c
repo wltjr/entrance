80e9f5698a86c5ce840ebb73d03dc69c813941aa
William L. Thomson Jr. <w@wltjr.com>
2026-04-06 10:21:03 -0400

    daemon: Drop call to entrance_session_display_set, no longer used

--- src/daemon/entrance.c
+++ src/daemon/entrance.c
@@ -426,7 +426,6 @@ _entrance_xservers_init()
         /* this needs to be modified to support per display/seat session */
         PT("session init for seat%d", i);
         entrance_session_start(i, entrance_display, vt);
-        entrance_session_display_set(entrance_display);
         entrance_session_cookie(i);
 
         pids[i] = entrance_xserver_start(i, _entrance_start_client, entrance_display, vt);
