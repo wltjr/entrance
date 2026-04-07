8aa9ad42c16fba376aeadca467c787aaafe84f0e
William L. Thomson Jr. <w@wltjr.com>
2026-04-07 10:11:02 -0400

    daemon: Remove void entrance_session_pid_set(pid_t pid) proto

--- src/daemon/entrance_session.h
+++ src/daemon/entrance_session.h
@@ -40,7 +40,6 @@ void entrance_sessions_shutdown();
 Eina_Bool entrance_session_authenticate(const char *login, const char *pwd);
 void entrance_session_close(Eina_Bool opened);
 Eina_Bool entrance_session_login(const char *command, Eina_Bool push);
-void entrance_session_pid_set(pid_t pid);
 pid_t entrance_session_pid_get(void);
 char *entrance_session_login_get(void);
 Eina_List *entrance_session_list_get(void);
