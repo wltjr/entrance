515b15330f974a1b34f1b1a49c421e3f329ef87a
William L. Thomson Jr. <w@wltjr.com>
2026-04-10 10:12:04 -0400

    bin: Add id param to entrance_connect proto for client id in server messages

--- src/bin/entrance_connect.h
+++ src/bin/entrance_connect.h
@@ -3,7 +3,16 @@
 
 typedef void (*Entrance_Connect_Auth_Cb)(void *data, const char *login, Eina_Bool granted);
 
-Eina_Bool entrance_connect(int port);
+/**
+ * @brief Establish an entrance client connection to the entrance server
+ * 
+ * @param id the id of the client, to identify which client it is to server
+ * @param port server port number, socket number
+ *
+ * @return Eina_Bool 
+ */
+Eina_Bool entrance_connect(int id, int port);
+
 void entrance_connect_auth_send(const char *login, const char *password, const char *session, Eina_Bool open_session);
 void entrance_connect_action_send(unsigned char id);
 void entrance_connect_conf_gui_send(const Entrance_Conf_Gui_Event *conf);
