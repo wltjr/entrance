aa9a49ab48cdc2fa705e8f2e167c520ce61adc74
William L. Thomson Jr. <w@wltjr.com>
2026-04-15 14:23:47 -0400

    bin: Fix bug, out-of-bound array access, switch to sizeof from number

--- src/bin/entrance_system.c
+++ src/bin/entrance_system.c
@@ -36,7 +36,7 @@ _entrance_system_distro_detect(void)
              /* Remove quotes */
              char *v = value;
              if (*v == '"') v++;
-             v[127] = '\0'; // guarantee null-terminated for strlen
+             v[sizeof(v) - 1] = '\0'; // guarantee null-terminated for strlen
              char *end = v + strlen(v) - 1;
              if (*end == '"') *end = '\0';
              
