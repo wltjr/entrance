ab4a357fb1be12fef26766a8e526fadc674158cd
William L. Thomson Jr. <w@wltjr.com>
2026-04-28 10:38:04 -0400

    utils: Add dirs to rm in /root & /run some for Xephyr, kill X/Xorg/Xephyr

--- utils/cleanup.sh
+++ utils/cleanup.sh
@@ -2,10 +2,12 @@
 #
 # Script to cleanup any mess resulting from failures during development
 
-killall Xorg
+killall Xephyr Xorg X
 
-rm -v /tmp/efreetd_dev_*
-rm -v /tmp/.ecore_service\|entrance\|*
+rm -v -r /root/.cache/e*
+rm -v -r /root/.run
+rm -v -r /run/.ecore*
+rm -v /tmp/efreetd_*
 rm -v -r /tmp/.X11-unix/
 rm -v /var/run/entrance.*
 rm -v /var/log/entrance.log
