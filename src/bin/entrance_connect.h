9871c5f8950339e7bc356cd7e2fdc324fdd0acaf
William L. Thomson Jr. <w@wltjr.com>
2026-04-29 10:32:04 -0400

    bin: Drop entrance_connect_conf_user_send proto

--- src/bin/entrance_connect.h
+++ src/bin/entrance_connect.h
@@ -15,7 +15,6 @@ Eina_Bool entrance_connect(int id, int port);
 
 void entrance_connect_auth_send(const char *login, const char *password, const char *session, Eina_Bool open_session);
 void entrance_connect_action_send(unsigned char id);
-void entrance_connect_conf_user_send(const Entrance_Login *el);
 void *entrance_connect_auth_cb_add(Entrance_Connect_Auth_Cb func, void *data);
 void entrance_connect_auth_cb_del(void *list);
 void entrance_connect_shutdown(void);
