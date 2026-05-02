bc053616e9085bfa4f61ca23b1977ae710038266
William L. Thomson Jr. <w@wltjr.com>
2026-05-02 10:22:03 -0400

    bin: Drop entrance_system_hostname_get proto, restore if needed

--- src/bin/entrance_system.h
+++ src/bin/entrance_system.h
@@ -1,7 +1,6 @@
 #ifndef ENTRANCE_SYSTEM_H_
 #define ENTRANCE_SYSTEM_H_
 
-const char *entrance_system_hostname_get(void);
 const char *entrance_system_distro_get(void);
 const char *entrance_system_logo_get(void);
 
