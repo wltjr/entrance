7b791838c7dd291e7e3a5c9cf9ced5c4c2d7dead
William L. Thomson Jr. <w@wltjr.com>
2026-05-02 10:27:13 -0400

    bin: Add entrance_system_shutdown() proto to free memory

--- src/bin/entrance_system.h
+++ src/bin/entrance_system.h
@@ -4,4 +4,9 @@
 const char *entrance_system_distro_get(void);
 const char *entrance_system_logo_get(void);
 
+/**
+ * @brief Shutdown system, free memory
+ */
+void entrance_system_shutdown();
+
 #endif /* ENTRANCE_SYSTEM_H_ */
