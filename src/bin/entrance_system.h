5221265b8a06130ea664f6dd8c686c3ccde2564e
William L. Thomson Jr. <w@wltjr.com>
2026-05-02 10:38:41 -0400

    bin: Add comments to entrance system getter functions

--- src/bin/entrance_system.h
+++ src/bin/entrance_system.h
@@ -1,7 +1,18 @@
 #ifndef ENTRANCE_SYSTEM_H_
 #define ENTRANCE_SYSTEM_H_
 
+/**
+ * @brief Get system distro name, detect distro from /etc/os-release (lazy init)
+ *
+ * @return const char* system distro name
+ */
 const char *entrance_system_distro_get(void);
+
+/**
+ * @brief Get distro logo path (lazy init)
+ * 
+ * @return const char* distro logo path
+ */
 const char *entrance_system_logo_get(void);
 
 /**
