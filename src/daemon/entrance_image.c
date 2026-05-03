b511659f8ecf3269d9fbc142b451f8b8a0d269ff
William L. Thomson Jr. <w@wltjr.com>
2026-05-03 17:09:23 -0400

    daemon: Modify _entrance_image_get signature drop unused params, remove code
    - All usage passed null, null as the last two params so they were never used
      the intentions of the underlying code in the function was unknown.
    - This also fixes a code smell since mask variable was not a string format
      passed to snprintf.

--- src/daemon/entrance_image.c
+++ src/daemon/entrance_image.c
@@ -2,7 +2,7 @@
 #include "entrance.h"
 
 static Eina_List *_entrance_image_readdir(const char *path);
-static Eina_List *_entrance_image_get(Eina_List *src, const char *stdfile, const char *mask);
+static Eina_List *_entrance_image_get(Eina_List *src);
 static char *_entrance_image_homedir_get(const char *usr);
 
 static Eina_List *
@@ -29,31 +29,15 @@ _entrance_image_readdir(const char *path)
 }
 
 static Eina_List *
-_entrance_image_get(Eina_List *src, const char *stdfile, const char *mask)
+_entrance_image_get(Eina_List *src)
 {
-   //If srdfile is NULL we will set the src string to file, if not we will set the stdfile. And the src as group.
    Eina_List *result = NULL;
    char *src_str;
    EINA_LIST_FREE(src, src_str)
      {
         Entrance_Image *img;
         img = calloc(1, sizeof(Entrance_Image));
-        if (stdfile)
-          {
-            if (mask)
-              {
-                 char path[PATH_MAX];
-
-                 snprintf(path, sizeof(path), mask, src_str);
-                 img->group = eina_stringshare_add(path);
-                 eina_stringshare_del(src_str);
-              }
-            else
-              img->group = src_str;
-            img->path = eina_stringshare_add(stdfile);
-          }
-        else
-          img->path = src_str;
+        img->path = src_str;
         result = eina_list_append(result,img);
      }
    return result;
@@ -82,16 +66,14 @@ Eina_List *
 entrance_image_system_icons(void)
 {
    return _entrance_image_get(
-      _entrance_image_readdir(PACKAGE_DATA_DIR"/images/icons/"),
-                              NULL, NULL);
+      _entrance_image_readdir(PACKAGE_DATA_DIR"/images/icons/"));
 }
 
 Eina_List *
  entrance_image_system_backgrounds(void)
 {
    return _entrance_image_get(
-      _entrance_image_readdir(PACKAGE_DATA_DIR"/images/backgrounds/"),
-                              NULL, NULL);
+      _entrance_image_readdir(PACKAGE_DATA_DIR"/images/backgrounds/"));
 }
 
 Eina_List *
@@ -103,7 +85,7 @@ entrance_image_user_icons(const char *username)
    homedir = _entrance_image_homedir_get(username);
    if (!homedir) return NULL;
    snprintf(path, sizeof(path),"%s/.config/entrance/images/icons/", homedir);
-   return _entrance_image_get(_entrance_image_readdir(path), NULL, NULL);
+   return _entrance_image_get(_entrance_image_readdir(path));
 }
 
 Eina_List *
@@ -115,5 +97,5 @@ entrance_image_user_backgrounds(const char *username)
    homedir = _entrance_image_homedir_get(username);
    if (!homedir) return NULL;
    snprintf(path, sizeof(path),"%s/.config/entrance/images/backgrounds/", homedir);
-   return _entrance_image_get(_entrance_image_readdir(path), NULL, NULL);
+   return _entrance_image_get(_entrance_image_readdir(path));
 }
