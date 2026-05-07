e0298d710e359609ffaba5e67b8881dcb4baf8b4
William L. Thomson Jr. <w@wltjr.com>
2026-05-07 10:33:21 -0400

    daemon: Drop entrance_history_user_update proto

--- src/daemon/entrance_history.h
+++ src/daemon/entrance_history.h
@@ -11,7 +11,6 @@ void entrance_history_check(const char *login, const char *session);
 void entrance_history_init(void);
 void entrance_history_shutdown(void);
 Eina_List *entrance_history_get(void);
-void entrance_history_user_update(const Entrance_Login *el);
 
 typedef struct _Entrance_History Entrance_History;
 
