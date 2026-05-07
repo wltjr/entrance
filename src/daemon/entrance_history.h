97c6710d8fe0b97b2fd63cf3312cd627623e8cca
William L. Thomson Jr. <w@wltjr.com>
2026-05-07 10:19:02 -0400

    daemon: Rename poorly named entrance_history_{push -> check} proto

--- src/daemon/entrance_history.h
+++ src/daemon/entrance_history.h
@@ -1,9 +1,15 @@
 #ifndef ENTRANCE_HISTORY_
 #define ENTRANCE_HISTORY_
 
+/**
+ * @brief Check if user and session exist in history, if not add, if so update.
+ *
+ * @param login     user login name to add or update in history
+ * @param session   user login session to add or update in history 
+ */
+void entrance_history_check(const char *login, const char *session);
 void entrance_history_init(void);
 void entrance_history_shutdown(void);
-void entrance_history_push(const char *login, const char *session);
 Eina_List *entrance_history_get(void);
 void entrance_history_user_update(const Entrance_Login *el);
 
