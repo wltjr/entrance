bdd871a2c8b9924723f433ab74e2ce1b32de5b68
William L. Thomson Jr. <w@wltjr.com>
2026-05-15 10:25:32 -0400

    tests: Remove main function from pam suite, moved to entrance_tests.c

--- src/tests/daemon/entrance_pam_tests.c
+++ src/tests/daemon/entrance_pam_tests.c
@@ -211,23 +211,3 @@ Suite *pam_suite(void)
 
     return s;
 }
-
-int main(void)
-{
-    int count = 1;
-    int failed;
-    Suite *s = pam_suite();
-    SRunner *runner = srunner_create(s);
-
-    eina_init();
-    eina_log_color_disable_set(EINA_FALSE); // force color logging
-    eina_log_threads_enable();
-    _entrance_log = eina_log_domain_register("entrance", EINA_COLOR_CYAN);
-    eina_log_domain_level_set("entrance", 5);
-
-    srunner_run_all(runner, CK_NORMAL);
-    failed = srunner_ntests_failed(runner);
-    srunner_free(runner);
-
-    return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
