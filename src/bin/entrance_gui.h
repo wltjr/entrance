24e8abaa75e800b039e19c853d57655e17676a2a
William L. Thomson Jr. <w@wltjr.com>
2026-05-26 10:16:03 -0400

    bin: Remove unused function proto entrance_gui_run
    - Likely to be used internally as a static function with _ preffix to
      split up entrance_gui_init, the code in the for loop and potentially
      other code.

--- src/bin/entrance_gui.h
+++ src/bin/entrance_gui.h
@@ -7,7 +7,6 @@ int entrance_gui_init(const char *theme);
 void entrance_gui_shutdown(void);
 Eina_List* entrance_gui_theme_icons(void);
 Eina_List* entrance_gui_theme_backgrounds(void);
-void entrance_gui_run(void);
 Evas_Object *entrance_gui_theme_get (Evas_Object *win, const char *group);
 void entrance_gui_auth_max_tries(void);
 void entrance_gui_xsession_set(Eina_List *xsessions);
